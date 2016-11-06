f1b7008240be89b7acce62a828342e8257e12e20
rlopescunha <rlopescunha17@gmail.com>
2016-11-06 17:38:07 +0000

    edit

--- views/pay_gateway.py
+++ views/pay_gateway.py
@@ -105,7 +105,12 @@ def transafe():
     data = {'user_id1': info['from_uuid'], 'user_id2': info['to_uuid'], 'transaction_id': trans_id, 'amount': info['price'],
             'description': info['object']['name'], 'callback': PAY_GATEWAY_CALLBACK_URL + "?id="+trans_id}
 
-    return redirect(requests.post(PAY_SERVICE_CREATE_PAYMENT, data=data, headers={'token_ID': PAY_SERVICE_TOKEN_ID}).url, 302)
+    response = requests.post(PAY_SERVICE_CREATE_PAYMENT, data=data, headers={'token_ID': PAY_SERVICE_TOKEN_ID})
+
+    if response.status_code != 302:
+        return response.message, response.status_code
+    else:
+        return redirect(requests.post(PAY_SERVICE_CREATE_PAYMENT, data=data, headers={'token_ID': PAY_SERVICE_TOKEN_ID}).url, 302)
 
 
 @pay_gateway.route("/callback", methods = ['GET'])
