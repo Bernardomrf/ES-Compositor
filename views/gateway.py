fbecf2dc00e91a53f2ee0a2ec249d409fda830ae
Bernardomrf <bernardomrferreira@ua.pt>
2016-11-14 11:12:16 +0000

    index completed

--- views/gateway.py
+++ views/gateway.py
@@ -39,9 +39,6 @@ def home():
     response = requests.get(TRANSACTIONS_STATS)
     number_transactions = response.json()["number_of_transactions"]
     total_amount = response.json()["total_value"]["price__sum"]
-    log.debug(number_transactions)
-    log.debug(number_transactions-response.json()["number_of_refunded"])
-    log.debug(float((number_transactions-response.json()["number_of_refunded"])))
 
     success_rate = int((float((number_transactions-response.json()["number_of_refunded"]))/float(number_transactions))*100)
     return render_template('login.html', users=user_count, transactions=number_transactions, amount=total_amount, success_rate=success_rate)
