3411e2b18c92ad43c908bd26b876cb7c9a85086d
Rodrigo Cunha <rlopescunha17@gmail.com>
2016-12-02 15:07:35 +0000

    App_token payments

--- settings.py
+++ settings.py
@@ -17,7 +17,7 @@ IAM_SIGNUP = "https://im.transafe.rafaelferreira.pt/login"
 IAM_LOGOUT = "https://im.transafe.rafaelferreira.pt/logout"
 IAM_USERS_COUNT = "https://im.transafe.rafaelferreira.pt/user/count"
 
-PAY_SERVICE_TOKEN_ID = "d457f395-a7f7-4dde-a2e2-3fa626c05f90"
+PAY_SERVICE_TOKEN_ID = "396428be-8bc7-415e-81d4-bbf698ab9d64"
 PAY_SERVICE_MYCARDS = "https://payments.transafe.rafaelferreira.pt/api/v1/cards/mycards/"
 PAY_SERVICE_CREATE_CARD = "https://payments.transafe.rafaelferreira.pt/api/v1/cards/create/"
 PAY_SERVICE_CREATE_PAYMENT = "https://payments.transafe.rafaelferreira.pt/api/v1/payments/create/"
